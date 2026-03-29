a5d920c7b830be0fdd201bd43b6dd92afff07827
amine-essafil <Jamesjoyone1@gmail.com>
2026-03-29 15:23:49 +0100

    feat: connect FoodItem to CartContext and meals.api with incrementReview on add

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import "./FoodItem.css";
+import { useCart } from "../../Context/CartContext";
 
 const FoodItem = ({ item }) => {
+  const { addToCart } = useCart();
+
+  const handleAdd = () => {
+    addToCart(item);
+  };
+
   return (
     <div className="food-item">
       <img src={item.image} alt={item.nom} className="food-item-image" />
@@ -9,7 +16,9 @@ const FoodItem = ({ item }) => {
         <p className="food-item-name">{item.nom}</p>
         <div className="food-item-details">
           <p className="food-item-price">${item.prix}</p>
-          <button className="food-item-add-btn">Add</button>
+          <button className="food-item-add-btn" onClick={handleAdd}>
+            Add
+          </button>
         </div>
       </div>
     </div>
