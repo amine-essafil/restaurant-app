ad16b78960602e1dcaf214157c2d95ac447f3704
Moujib <Moujibch10@gmail.com>
2026-03-31 10:52:22 +0100

    feat :add login ,AuthContext logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,32 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
+import { AuthProvider } from "./context/AuthContext";  
+
 import LandingPage from "./pages/LandingPage/LandingPage";
 import MenuPage from "./pages/MenuPage/MenuPage";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <CartProvider>
-      <BrowserRouter>
-        <div className="app-container">
+    <AuthProvider> 
+      <CartProvider>
+        <BrowserRouter>
 
-          <Navbar />
+          <div className="app-container">
+            <Navbar />
 
-          <main>
-            <Routes>
-              <Route path="/" element={<LandingPage />} />
-              <Route path="/menu" element={<MenuPage />} />
-            </Routes>
-          </main>
+            <main>
+              <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/menu" element={<MenuPage />} />
+              </Routes>
+            </main>
 
-        </div>
-      </BrowserRouter>
-    </CartProvider>
+          </div>
+
+        </BrowserRouter>
+      </CartProvider>
+    </AuthProvider>
   );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,64 @@
-import React from "react";
 import "./Navbar.css";
-import { Link } from "react-router-dom";
-import { useCart } from "../../context/CartContext";
+import { useAuth } from "../../context/AuthContext";
+import {  Link } from "react-router-dom";
 import Logo from "../../assets/images/logo.png";
+ 
+const FaUserCircle = () => (
+  <svg
+    stroke="currentColor"
+    fill="currentColor"
+    strokeWidth="0"
+    viewBox="0 0 16 16"
+    className="profile-icon"
+    height="1em"
+    width="1em"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"></path>
+    <path
+      fillRule="evenodd"
+      d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
+    ></path>
+  </svg>
+);
+
+const FiMenu = () => (
+  <svg
+    stroke="currentColor"
+    fill="none"
+    strokeWidth="2"
+    viewBox="0 0 24 24"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    height="1em"
+    width="1em"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <line x1="3" y1="12" x2="21" y2="12"></line>
+    <line x1="3" y1="6" x2="21" y2="6"></line>
+    <line x1="3" y1="18" x2="21" y2="18"></line>
+  </svg>
+);
+
+const FiX = () => (
+  <svg
+    stroke="currentColor"
+    fill="none"
+    strokeWidth="2"
+    viewBox="0 0 24 24"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    height="1em"
+    width="1em"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <line x1="18" y1="6" x2="6" y2="18"></line>
+    <line x1="6" y1="6" x2="18" y2="18"></line>
+  </svg>
+);
 
 const Navbar = () => {
-  const { cartCount } = useCart();
+  const { isLoggedIn, user } = useAuth();
 
   return (
     <nav className="navbar">
@@ -15,19 +68,40 @@ const Navbar = () => {
           <img src={Logo} alt="FoodExpress Logo" />
         </Link>
 
-        <ul className="navbar-links">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/menu">Menu</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
+         <ul >
+          <li>
+            <Link to="/" >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/menu" >
+              Menu
+            </Link>
+          </li>
+          <li>
+            <a >Offers</a>
+          </li>
+          <li>
+            <Link to="/contact" >
+              Contact
+            </Link>
+          </li>
+            
         </ul>
 
         <div className="navbar-actions">
-          <Link to="/cart" className="navbar-cart">
-            <img src="/src/assets/icons/cart.svg" className="cart-icon" />
-            {cartCount > 0 && (
-              <span className="cart-count">{cartCount}</span>
-            )}
-          </Link>
+
+          {isLoggedIn ? (
+            <span className="navbar-profile-btn">
+              {user?.name || "User"}
+            </span>
+          ) : (
+            <Link to="/login" className="navbar-signin-btn">
+              Sign In
+            </Link>
+          )}
+
         </div>
 
       </div>
