72352592bbbbd75e7fdeda78ab048cdafc454b6a
Moujib <Moujibch10@gmail.com>
2026-05-29 23:18:19 +0100

    fix(auth): correctly read JWT token from query params and handle redirect safely

--- src/pages/LoginSuccess/LoginSuccess.jsx
+++ src/pages/LoginSuccess/LoginSuccess.jsx
@@ -2,19 +2,19 @@ import { useEffect } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 
 export default function LoginSuccess() {
- const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
 
-useEffect(() => { 
-  const token = new URLSearchParams(window.location.search).get("token");
+  useEffect(() => {
+    const token = searchParams.get("token");
 
-  if (token) {
-    localStorage.setItem("token", token);
-    navigate("/home");
-  }else {
-      navigate("/login"); 
+    if (token) {
+      localStorage.setItem("token", token);
+      navigate("/", { replace: true });
+    } else {
+      navigate("/login", { replace: true });
     }
-}, []);
-
+  }, [navigate, searchParams]);
 
   return <h2>✅ Login réussi</h2>;
 }
