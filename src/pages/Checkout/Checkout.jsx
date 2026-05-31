e6246ef0f240f9922eee730002c67c56114326e4
amine-essafil <Jamesjoyone1@gmail.com>
2026-05-31 21:30:25 +0100

    feat(checkout): add order total calculation logic

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -11,12 +11,20 @@ const CheckoutPage = () => {
   const [phone, setPhone] = useState("");
   const [address, setAddress] = useState("");
   const [instructions, setInstructions] = useState("");
-  
+
   useEffect(() => {
     if (cartItems.length === 0) {
       navigate("/cart");
     }
   }, [cartItems, navigate]);
+
+  const subtotal = cartItems.reduce(
+  (sum, item) => sum + item.plat.prix * item.quantite,
+  0
+);
+
+const deliveryFee = 5;
+const total = subtotal + deliveryFee;
 
   return (
     <div className="checkout-page-container">
