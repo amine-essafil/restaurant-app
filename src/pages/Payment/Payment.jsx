042d81bfccaa883112f3b5e520836c7deb4cb9a6
amine-essafil <Jamesjoyone1@gmail.com>
2026-06-01 15:01:52 +0100

    chore(payment): set up initial Payment page structure

---
+++ src/pages/Payment/Payment.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useAuth } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+import { useCart } from "../../context/CartContext";
+import { useOrder } from "../../context/OrderContext";
+import "./Payment.css";
+
+return (
+    <div className="payment-page-container">
+
+    </div>
+  );
+};
+
+export default PaymentPage;
