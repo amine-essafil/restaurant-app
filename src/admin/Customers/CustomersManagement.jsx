e2700509da6d05a5653ca4e3e69981023c49a3ae
amine-essafil <Jamesjoyone1@gmail.com>
2026-06-04 23:34:03 +0100

    chore(customers): set up initial Customers page structure

---
+++ src/admin/Customers/CustomersManagement.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useMemo, useEffect } from "react";
+import { Link, Navigate, useLocation, useNavigate } from "react-router-dom";
+import {
+  FaUserCircle,
+  FaSearch,
+  FaPlus,
+  FaTrash,
+  FaEye,
+  FaTimes,
+  FaCheck,
+  FaPhone,
+  FaEnvelope,
+  FaMapMarkerAlt,
+  FaCalendar,
+  FaShoppingBag,
+  FaDollarSign,
+  FaStar,
+  FaFilter,
+  FaDownload,
+  FaUpload,
+  FaBan,
+  FaCheckCircle,
+  FaCrown,
+  FaEdit,
+  FaHeart,
+  FaChartLine,
+  FaGift,
+  FaClock,
+  FaCommentDots,
+  FaUtensils,
+  FaTruck,
+  FaMotorcycle,
+  FaFileAlt,
+  FaBars,
+  FaInfoCircle,
+  FaUserEdit,
+  FaTrashAlt,
+  FaUserLock,
+} from "react-icons/fa";
+import { SquarePen } from "lucide-react";
+import "./CustomersManagement.css";
+import { ClientApi } from "../../ClientApi/ClientApi";
+
+const CustomersManagement = () => {
+  return (
+    <div className="customers-management-container">
+    </div>
+  );
+};
+
+export default CustomersManagement;
