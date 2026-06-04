141671714a2f79f81bdc99e4a5ac67a8c1b79ba8
Moujib <Moujibch10@gmail.com>
2026-06-04 15:16:32 +0100

    feat(drivers): add drivers api's

--- src/api/Drivers.api.js
+++ src/api/Drivers.api.js
@@ -5,4 +5,27 @@ export const availableDrivers = (params = {}) =>
   client.get(`/api/v1/drivers/available/`,{
                 params: params 
             });
+    
+// --- Drivers ---
+export const getDrivers = () =>
+  client.get(`/api/v1/drivers`);    
 
+// --- Driver Dashboard ---
+export const DriverDashboard = () =>
+  client.get(`/api/v1/drivers/dashboard`);
+
+// --- Drivers ---
+export const CreateDriver = (formData) =>
+  client.post(`/api/v1/drivers`, formData);
+
+// --- Driver Update ---
+export const DriverUpdate = (driverId, formData) =>
+  client.put(`/api/v1/drivers/${driverId}`, formData);
+
+// --- Driver Delete ---
+export const DriverDelete = (driverId) =>
+  client.delete(`/api/v1/drivers/${driverId}`);
+
+// --- Driver Update Status ---
+export const DriverUpdateStatus = (driverId, statut) =>
+  client.patch(`/api/v1/drivers/${driverId}/status`, statut);
