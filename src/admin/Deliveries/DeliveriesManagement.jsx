1c49fa96fc58ae86adc0899cf4eee36d0f68b588
amine-essafil <Jamesjoyone1@gmail.com>
2026-06-07 19:45:53 +0100

    chore(deliveries): set up initial Deliveries page structure

---
+++ src/admin/Deliveries/DeliveriesManagement.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useMemo, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import {
+  FaUtensils,
+  FaBars,
+  FaTimes,
+  FaChartLine,
+  FaTruck,
+  FaUsers,
+  FaMotorcycle,
+  FaFileAlt,
+  FaShoppingBag,
+  FaPhone,
+  FaMapMarkerAlt,
+  FaSearch,
+  FaChevronDown,
+  FaChevronUp,
+  FaCheck,
+  FaExclamationCircle,
+} from "react-icons/fa";
+import "./DeliveriesManagement.css";
+import { ClientApi } from "../../ClientApi/ClientApi";
+
+const DeliveriesManagement = () => {
+  return (
+    <div className="deliveries-management-container">
+    </div>
+  );
+};
+
+export default DeliveriesManagement;
